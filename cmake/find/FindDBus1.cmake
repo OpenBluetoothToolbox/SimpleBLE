c4de11903a77bc320e8d6b7c3b326123a6de5c12
Kevin Dewald <kdewald@itba.edu.ar>
2022-08-21 14:34:38 -0700

    Test if DBus is properly imported

--- cmake/find/FindDBus1.cmake
+++ cmake/find/FindDBus1.cmake
@@ -4,29 +4,23 @@ find_package(DBus1 CONFIG QUIET)
 # If DBus1 was not found, try to find it as MODULE
 if(NOT DBus1_FOUND)
 
-    # - Config file for the DBus1 package
-    # It defines the following variables
-    #  DBus1_FOUND - Flag for indicating that DBus1 package has been found
-    #  DBus1_DEFINITIONS  - compile definitions for DBus1 [1]
-    #  DBus1_INCLUDE_DIRS - include directories for DBus1 [1]
-    #  DBus1_LIBRARIES    - cmake targets to link against
-
     find_package(PkgConfig REQUIRED)
     pkg_search_module(PC_DBUS REQUIRED dbus-1)
 
-    set(DBus1_LIBRARIES dbus-1)
-    set(DBus1_INCLUDE_DIRS "${DBus1_INCLUDE_DIR}" "${DBus1_ARCH_INCLUDE_DIR}")
+    set(DBus1_LIBRARIES ${PC_DBUS_LIBRARIES})
+    set(DBus1_INCLUDE_DIRS ${PC_DBUS_INCLUDE_DIRS})
 
     # setup imported target
     add_library(dbus-1 SHARED IMPORTED)
-    set_property(TARGET dbus-1 APPEND PROPERTY IMPORTED_LOCATION ${DBus1_LINK_LIBRARIES})
+    set_property(TARGET dbus-1 APPEND PROPERTY IMPORTED_LOCATION ${PC_DBUS_LINK_LIBRARIES})
     set_property(TARGET dbus-1 APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${DBus1_INCLUDE_DIRS})
-    set_property(TARGET dbus-1 APPEND PROPERTY INTERFACE_COMPILE_DEFINITIONS ${DBus1_DEFINITIONS})
 
-    get_cmake_property(_variableNames VARIABLES)
-    list (SORT _variableNames)
-    foreach (_variableName ${_variableNames})
-        message(STATUS "${_variableName}=${${_variableName}}")
-    endforeach()
+    set(DBus1_FOUND ${PC_DBUS_FOUND})
+
+    # get_cmake_property(_variableNames VARIABLES)
+    # list (SORT _variableNames)
+    # foreach (_variableName ${_variableNames})
+    #     message(STATUS "${_variableName}=${${_variableName}}")
+    # endforeach()
 
 endif()
