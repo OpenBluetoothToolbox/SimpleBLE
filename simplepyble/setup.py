77f3a737d4d96fe13f5d3caf501acc42c64edf66
Kevin Dewald <kdewald@itba.edu.ar>
2022-08-21 18:52:23 -0700

    Add build flags to setup.py

--- simplepyble/setup.py
+++ simplepyble/setup.py
@@ -9,6 +9,13 @@ here = pathlib.Path(__file__).parent.resolve()
 # long_description = (here / "README.md").read_text(encoding="utf-8")
 long_description = "hello"
 
+cmake_options = []
+if sys.platform == "win32":
+    cmake_options.append("-GVisual Studio 16 2019")
+    cmake_options.append("-DCMAKE_SYSTEM_VERSION=10.0.19041.0")
+elif sys.platform.startswith("darwin"):
+    cmake_options.append("-DCMAKE_OSX_DEPLOYMENT_TARGET=10.15")
+
 # The information here can also be placed in setup.cfg - better separation of
 # logic and declaration, and simpler if you include description/version in a file.
 setuptools.setup(
@@ -24,7 +31,7 @@ setuptools.setup(
             name="simplepyble",
             cmake_depends_on=["pybind11"],
             disable_editable=True,
-            cmake_configure_options=['-G','Visual Studio 16 2019'] if sys.platform=='win32' else []
+            cmake_configure_options=cmake_options
         )
     ],
     cmdclass={
