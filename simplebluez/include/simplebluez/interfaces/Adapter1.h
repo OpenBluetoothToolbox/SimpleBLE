74358b3aebec8790be37d95b2ca7d6b66ffb5c94
Kevin Dewald <kdewald@itba.edu.ar>
2022-09-08 21:48:03 -0700

    Added support for proper discovery filters in SimpleBluez.

--- simplebluez/include/simplebluez/interfaces/Adapter1.h
+++ simplebluez/include/simplebluez/interfaces/Adapter1.h
@@ -2,14 +2,27 @@
 
 #include <simpledbus/advanced/Interface.h>
 
+#include <optional>
 #include <string>
+#include <vector>
 
 namespace SimpleBluez {
 
 class Adapter1 : public SimpleDBus::Interface {
   public:
-    typedef enum { AUTO, BREDR, LE } DiscoveryFilter;
+    // ----- TYPES -----
+    struct DiscoveryFilter {
+        typedef enum { AUTO, BREDR, LE } TransportType;
+        std::vector<std::string> UUIDs = {};
+        std::optional<int16_t> RSSI;
+        std::optional<uint16_t> Pathloss;
+        TransportType Transport = TransportType::AUTO;
+        bool DuplicateData = true;
+        bool Discoverable = false;
+        std::string Pattern = "";
+    };
 
+    // ----- CONSTRUCTORS -----
     Adapter1(std::shared_ptr<SimpleDBus::Connection> conn, std::string path);
     virtual ~Adapter1() = default;
 
@@ -19,7 +32,6 @@ class Adapter1 : public SimpleDBus::Interface {
     void StopDiscovery();
     void SetDiscoveryFilter(DiscoveryFilter filter);
     SimpleDBus::Holder GetDiscoveryFilters();
-    // void RemoveDevice(SimpleDBus::Holder device); // TODO: Implement
 
     // ----- PROPERTIES -----
     bool Discovering(bool refresh = true);
