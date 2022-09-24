4503cdde76477440c00273105e699874ed0b03f3
Kevin Dewald <kdewald@itba.edu.ar>
2022-09-24 16:13:15 -0700

    See if we can get RTD working

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ author = 'Kevin Dewald'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.napoleon",
@@ -56,7 +57,7 @@ subprocess.call(["pip", "install", "."], cwd=(root / "simplepyble"))
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
