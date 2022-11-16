071e3dba5e9a48a2d525a3643777a27801c6e522
Cameron Miller <cameron.miller@homeaware.com>
2022-11-15 19:54:53 -0600

    Update example

--- examples/simpleble/cpp/scan/scan.cpp
+++ examples/simpleble/cpp/scan/scan.cpp
@@ -40,6 +40,8 @@ int main() {
 
         std::cout << "[" << i << "] " << peripheral_string << " " << connectable_string << std::endl;
 
+        std::cout << "Tx Power: " << peripherals[i].tx_power() " dBm" << std::endl;
+
         std::vector<SimpleBLE::Service> services = peripherals[i].services();
         for (auto& service : services) {
             std::cout << "    Service: " << service.uuid() << std::endl;
