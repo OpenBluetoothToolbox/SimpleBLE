4ce3b08be4a7702f31cdb2952a3bc37bbee7f541
Kevin Dewald <kdewald@itba.edu.ar>
2022-12-21 18:25:41 -0800

    Moved external modules into a common folder

--- simplebluez/include/simplebluez/interfaces/Agent1.h
+++ simplebluez/include/simplebluez/interfaces/Agent1.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <simpledbus/advanced/Interface.h>
-#include <simpledbus/external/kvn_safe_callback.hpp>
+#include <external/kvn_safe_callback.hpp>
 
 #include <cstdint>
 #include <string>
