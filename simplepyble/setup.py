9c2609d41e4f9bee5e6a7128fd764f60b3f5872a
Kevin Dewald <5274600+kdewald@users.noreply.github.com>
2023-01-11 18:25:26 -0800

    Removed duplicate artifact entry, updated version tag for release

--- simplepyble/setup.py
+++ simplepyble/setup.py
@@ -23,7 +23,7 @@ root = here.parent.resolve()
 # Generate the version string
 # TODO: Make the dev portion smarter by looking at tags.
 version_str = (root / "VERSION").read_text(encoding="utf-8").strip()
-version_str += ".dev2" # ! Ensure it matches the intended release version!
+version_str += ".dev3" # ! Ensure it matches the intended release version!
 
 # Get the long description from the README file
 long_description = (here / "README.rst").read_text(encoding="utf-8")
