1426e3b4ca5279c1e2373dcee4e82b18375f97ed
Kevin Dewald <5274600+kdewald@users.noreply.github.com>
2023-01-11 20:39:17 -0800

    Fixed missing binary in Windows Py builds. Add artifacts to jobs (#170)
    * Fixed missing binary in Windows Py builds. Add artifacts to jobs
    
    * Removed duplicate artifact entry, updated version tag for release

--- simplepyble/setup.py
+++ simplepyble/setup.py
@@ -6,7 +6,7 @@ import pybind11
 
 def exclude_unnecessary_files(cmake_manifest):
     def is_necessary(name):
-        is_necessary = name.endswith(".so") or name.endswith(".dylib") or name.endswith("py")
+        is_necessary = name.endswith(".so") or name.endswith(".dylib") or name.endswith("py") or name.endswith("pyd")
         print(f"Parsing file: {name} - {is_necessary}")
         return is_necessary
 
@@ -23,7 +23,7 @@ root = here.parent.resolve()
 # Generate the version string
 # TODO: Make the dev portion smarter by looking at tags.
 version_str = (root / "VERSION").read_text(encoding="utf-8").strip()
-version_str += ".dev2" # ! Ensure it matches the intended release version!
+version_str += ".dev3" # ! Ensure it matches the intended release version!
 
 # Get the long description from the README file
 long_description = (here / "README.rst").read_text(encoding="utf-8")
