1216f6e8dda70f0213b6787fac67a202d3d79837
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-06-09 18:05:43 -0400

    dbus: more fixes

--- cmake/find/FindDBus1.cmake
+++ cmake/find/FindDBus1.cmake
@@ -1,6 +1,6 @@
 find_package(PkgConfig QUIET)
 if(PKG_CONFIG_FOUND)
-    pkg_check_modules(_dbus_hint QUIET libdbus)
+    pkg_check_modules(_dbus_hint QUIET dbus-1)
 endif()
 
 find_path(DBus1_INCLUDE_DIRS
@@ -19,7 +19,25 @@ if(NOT DBus1_INCLUDE_DIRS)
     return()
 endif()
 
+find_path(DBus1_ARCH_INCLUDE_DIRS
+  NAMES
+    dbus/dbus-arch-deps.h
+  HINTS
+    ${_dbus_hint_INCLUDE_DIRS}
+  PATH_SUFFIXES
+    include/
+    include/dbus-1.0/
+    dbus-1.0/
+    lib/include/
+    lib/include/dbus-1.0/
+    lib/dbus-1.0/
+    lib64/include/
+    lib64/include/dbus-1.0/
+    lib64/dbus-1.0/
+)
+
 # setup imported target
 add_library(dbus-1 INTERFACE)
-set_property(TARGET dbus-1 APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${DBus1_INCLUDE_DIRS})
+add_library(dbus-1::dbus-1 ALIAS dbus-1)
+set_property(TARGET dbus-1 APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${DBus1_INCLUDE_DIRS} ${DBus1_ARCH_INCLUDE_DIRS})
 set(DBus1_FOUND 1)
