a994534eb74752db3077ee6602f4e4dd5c985514
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-06-09 18:34:38 -0400

    wip

--- cmake/find/FindDBus1.cmake
+++ cmake/find/FindDBus1.cmake
@@ -28,13 +28,15 @@ find_path(DBus1_ARCH_INCLUDE_DIRS
     include/
     include/dbus-1.0/
     dbus-1.0/
-    lib/include/
-    lib/include/dbus-1.0/
-    lib/dbus-1.0/
-    lib64/include/
-    lib64/include/dbus-1.0/
-    lib64/dbus-1.0/
+    lib/dbus-1.0/include/
+    lib64/dbus-1.0/include/
+    lib32/dbus-1.0/include/
 )
+
+if(NOT DBus1_ARCH_INCLUDE_DIRS)
+    set(DBus1_FOUND 0)
+    return()
+endif()
 
 # setup imported target
 add_library(dbus-1 INTERFACE)
