4d3870b5117822c2fc6f2868258aa544dc354316
Kevin Dewald <kdewald@itba.edu.ar>
2024-10-06 16:38:58 -0700

    Exposed all LEAdvertisement1 properties on the CustomAdvertisement class.

--- simplebluez/include/simplebluez/interfaces/LEAdvertisement1.h
+++ simplebluez/include/simplebluez/interfaces/LEAdvertisement1.h
@@ -1,10 +1,10 @@
 #pragma once
 #include <simpledbus/advanced/Interface.h>
+#include <cstdint>
+#include <map>
 #include <optional>
 #include <string>
 #include <vector>
-#include <map>
-#include <cstdint>
 
 namespace SimpleBluez {
 
@@ -17,32 +17,64 @@ class LEAdvertisement1 : public SimpleDBus::Interface {
 
     // ----- PROPERTIES -----
 
-    void SetType(const std::string& type);
-    void SetServiceUUIDs(const std::vector<std::string>& uuids);
-    void SetManufacturerData(const std::map<uint16_t, std::vector<uint8_t>>& data);
-    void SetServiceData(const std::map<std::string, std::vector<uint8_t>>& data);
-    void SetSolicitUUIDs(const std::vector<std::string>& uuids);
-    void SetData(const std::map<uint8_t, std::vector<uint8_t>>& data);
-    void SetDiscoverable(bool discoverable);
-    void SetDiscoverableTimeout(uint16_t timeout);
-    void SetIncludes(const std::vector<std::string>& includes);
-    void SetLocalName(const std::string& name);
-    void SetAppearance(uint16_t appearance);
-    void SetDuration(uint16_t duration);
-    void SetTimeout(uint16_t timeout);
-    void SetSecondaryChannel(const std::string& channel);
-    void SetMinInterval(uint32_t interval);
-    void SetMaxInterval(uint32_t interval);
-    void SetTxPower(int16_t power);
-    void SetIncludeTxPower(bool include);
+    std::string Type();
+    void Type(const std::string& type);
 
+    std::vector<std::string> ServiceUUIDs();
+    void ServiceUUIDs(const std::vector<std::string>& uuids);
+
+    std::map<uint16_t, std::vector<uint8_t>> ManufacturerData();
+    void ManufacturerData(const std::map<uint16_t, std::vector<uint8_t>>& data);
+
+    std::map<std::string, std::vector<uint8_t>> ServiceData();
+    void ServiceData(const std::map<std::string, std::vector<uint8_t>>& data);
+
+    std::vector<std::string> SolicitUUIDs();
+    void SolicitUUIDs(const std::vector<std::string>& uuids);
+
+    std::map<uint8_t, std::vector<uint8_t>> Data();
+    void Data(const std::map<uint8_t, std::vector<uint8_t>>& data);
+
+    bool Discoverable();
+    void Discoverable(bool discoverable);
+
+    uint16_t DiscoverableTimeout();
+    void DiscoverableTimeout(uint16_t timeout);
+
+    std::vector<std::string> Includes();
+    void Includes(const std::vector<std::string>& includes);
+
+    std::string LocalName();
+    void LocalName(const std::string& name);
+
+    uint16_t Appearance();
+    void Appearance(uint16_t appearance);
+
+    uint16_t Duration();
+    void Duration(uint16_t duration);
+
+    uint16_t Timeout();
+    void Timeout(uint16_t timeout);
+
+    std::string SecondaryChannel();
+    void SecondaryChannel(const std::string& channel);
+
+    uint32_t MinInterval();
+    void MinInterval(uint32_t interval);
+
+    uint32_t MaxInterval();
+    void MaxInterval(uint32_t interval);
+
+    int16_t TxPower();
+    void TxPower(int16_t power);
+
+    bool IncludeTxPower();
+    void IncludeTxPower(bool include);
     // ----- METHODS -----
 
     void Release();
 
   protected:
-
-
   private:
 };
 
--- simplebluez/src/interfaces/LEAdvertisement1.cpp
+++ simplebluez/src/interfaces/LEAdvertisement1.cpp
@@ -5,35 +5,18 @@ using namespace SimpleBluez;
 LEAdvertisement1::LEAdvertisement1(std::shared_ptr<SimpleDBus::Connection> conn, SimpleDBus::Proxy* proxy)
     : SimpleDBus::Interface(conn, proxy, "org.bluez.LEAdvertisement1") {
 
-    // Set all fields to null or empty values
-    SetType("peripheral"); // This is the only required field, so we set a default
-    // SetServiceUUIDs({}); // Empty vector
-
-    std::map<uint16_t, std::vector<uint8_t>> data;
-    data[0x1024] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05};
-    SetManufacturerData(data); // Empty map
-    // SetServiceData({}); // Empty map
-    // SetSolicitUUIDs({}); // Empty vector
-    // SetData({}); // Empty map
-    // SetDiscoverable(false);
-    // SetDiscoverableTimeout(0);
-    // SetIncludes({}); // Empty vector
-    SetLocalName("SimpleBluez");
-    // SetAppearance(0);
-    // SetDuration(180);
-    SetTimeout(180);
-    // SetSecondaryChannel("");
-    // SetMinInterval(0);
-    // SetMaxInterval(0);
-    // SetTxPower(0);
-    // SetIncludeTxPower(false);
+    Type("peripheral"); // This is the only required field, so we set a default
 }
 
-void LEAdvertisement1::SetType(const std::string& type) {
+void LEAdvertisement1::Type(const std::string& type) {
     _properties["Type"] = SimpleDBus::Holder::create_string(type);
 }
 
-void LEAdvertisement1::SetServiceUUIDs(const std::vector<std::string>& uuids) {
+std::string LEAdvertisement1::Type() {
+    return _properties["Type"].get_string();
+}
+
+void LEAdvertisement1::ServiceUUIDs(const std::vector<std::string>& uuids) {
     SimpleDBus::Holder uuids_holder = SimpleDBus::Holder::create_array();
     for (const auto& uuid : uuids) {
         uuids_holder.array_append(SimpleDBus::Holder::create_string(uuid));
@@ -41,7 +24,16 @@ void LEAdvertisement1::SetServiceUUIDs(const std::vector<std::string>& uuids) {
     _properties["ServiceUUIDs"] = uuids_holder;
 }
 
-void LEAdvertisement1::SetManufacturerData(const std::map<uint16_t, std::vector<uint8_t>>& data) {
+std::vector<std::string> LEAdvertisement1::ServiceUUIDs() {
+    std::vector<std::string> uuids;
+    auto uuids_holder = _properties["ServiceUUIDs"];
+    for (const auto& uuid : uuids_holder.get_array()) {
+        uuids.push_back(uuid.get_string());
+    }
+    return uuids;
+}
+
+void LEAdvertisement1::ManufacturerData(const std::map<uint16_t, std::vector<uint8_t>>& data) {
     SimpleDBus::Holder data_holder = SimpleDBus::Holder::create_dict();
     for (const auto& [key, value] : data) {
         SimpleDBus::Holder value_holder = SimpleDBus::Holder::create_array();
@@ -53,7 +45,20 @@ void LEAdvertisement1::SetManufacturerData(const std::map<uint16_t, std::vector<
     _properties["ManufacturerData"] = data_holder;
 }
 
-void LEAdvertisement1::SetServiceData(const std::map<std::string, std::vector<uint8_t>>& data) {
+std::map<uint16_t, std::vector<uint8_t>> LEAdvertisement1::ManufacturerData() {
+    std::map<uint16_t, std::vector<uint8_t>> data;
+    auto data_holder = _properties["ManufacturerData"];
+    for (const auto& [key, value] : data_holder.get_dict_uint16()) {
+        std::vector<uint8_t> bytes;
+        for (const auto& byte : value.get_array()) {
+            bytes.push_back(byte.get_byte());
+        }
+        data[key] = bytes;
+    }
+    return data;
+}
+
+void LEAdvertisement1::ServiceData(const std::map<std::string, std::vector<uint8_t>>& data) {
     SimpleDBus::Holder data_holder = SimpleDBus::Holder::create_dict();
     for (const auto& [key, value] : data) {
         SimpleDBus::Holder value_holder = SimpleDBus::Holder::create_array();
@@ -65,16 +70,20 @@ void LEAdvertisement1::SetServiceData(const std::map<std::string, std::vector<ui
     _properties["ServiceData"] = data_holder;
 }
 
-void LEAdvertisement1::SetIncludeTxPower(bool include) {
-    _properties["IncludeTxPower"] = SimpleDBus::Holder::create_boolean(include);
+std::map<std::string, std::vector<uint8_t>> LEAdvertisement1::ServiceData() {
+    std::map<std::string, std::vector<uint8_t>> data;
+    auto data_holder = _properties["ServiceData"];
+    for (const auto& [key, value] : data_holder.get_dict_string()) {
+        std::vector<uint8_t> bytes;
+        for (const auto& byte : value.get_array()) {
+            bytes.push_back(byte.get_byte());
+        }
+        data[key] = bytes;
+    }
+    return data;
 }
 
-void LEAdvertisement1::Release() {
-    // This method is called when the advertisement is unregistered
-    // Implement any cleanup logic here
-}
-
-void LEAdvertisement1::SetSolicitUUIDs(const std::vector<std::string>& uuids) {
+void LEAdvertisement1::SolicitUUIDs(const std::vector<std::string>& uuids) {
     SimpleDBus::Holder uuids_holder = SimpleDBus::Holder::create_array();
     for (const auto& uuid : uuids) {
         uuids_holder.array_append(SimpleDBus::Holder::create_string(uuid));
@@ -82,7 +91,16 @@ void LEAdvertisement1::SetSolicitUUIDs(const std::vector<std::string>& uuids) {
     _properties["SolicitUUIDs"] = uuids_holder;
 }
 
-void LEAdvertisement1::SetData(const std::map<uint8_t, std::vector<uint8_t>>& data) {
+std::vector<std::string> LEAdvertisement1::SolicitUUIDs() {
+    std::vector<std::string> uuids;
+    auto uuids_holder = _properties["SolicitUUIDs"];
+    for (const auto& uuid : uuids_holder.get_array()) {
+        uuids.push_back(uuid.get_string());
+    }
+    return uuids;
+}
+
+void LEAdvertisement1::Data(const std::map<uint8_t, std::vector<uint8_t>>& data) {
     SimpleDBus::Holder data_holder = SimpleDBus::Holder::create_dict();
     for (const auto& [key, value] : data) {
         SimpleDBus::Holder value_holder = SimpleDBus::Holder::create_array();
@@ -94,15 +112,36 @@ void LEAdvertisement1::SetData(const std::map<uint8_t, std::vector<uint8_t>>& da
     _properties["Data"] = data_holder;
 }
 
-void LEAdvertisement1::SetDiscoverable(bool discoverable) {
+std::map<uint8_t, std::vector<uint8_t>> LEAdvertisement1::Data() {
+    std::map<uint8_t, std::vector<uint8_t>> data;
+    auto data_holder = _properties["Data"];
+    for (const auto& [key, value] : data_holder.get_dict_uint8()) {
+        std::vector<uint8_t> bytes;
+        for (const auto& byte : value.get_array()) {
+            bytes.push_back(byte.get_byte());
+        }
+        data[key] = bytes;
+    }
+    return data;
+}
+
+void LEAdvertisement1::Discoverable(bool discoverable) {
     _properties["Discoverable"] = SimpleDBus::Holder::create_boolean(discoverable);
 }
 
-void LEAdvertisement1::SetDiscoverableTimeout(uint16_t timeout) {
+bool LEAdvertisement1::Discoverable() {
+    return _properties["Discoverable"].get_boolean();
+}
+
+void LEAdvertisement1::DiscoverableTimeout(uint16_t timeout) {
     _properties["DiscoverableTimeout"] = SimpleDBus::Holder::create_uint16(timeout);
 }
 
-void LEAdvertisement1::SetIncludes(const std::vector<std::string>& includes) {
+uint16_t LEAdvertisement1::DiscoverableTimeout() {
+    return _properties["DiscoverableTimeout"].get_uint16();
+}
+
+void LEAdvertisement1::Includes(const std::vector<std::string>& includes) {
     SimpleDBus::Holder includes_holder = SimpleDBus::Holder::create_array();
     for (const auto& include : includes) {
         includes_holder.array_append(SimpleDBus::Holder::create_string(include));
@@ -110,34 +149,88 @@ void LEAdvertisement1::SetIncludes(const std::vector<std::string>& includes) {
     _properties["Includes"] = includes_holder;
 }
 
-void LEAdvertisement1::SetLocalName(const std::string& name) {
+std::vector<std::string> LEAdvertisement1::Includes() {
+    std::vector<std::string> includes;
+    auto includes_holder = _properties["Includes"];
+    for (const auto& include : includes_holder.get_array()) {
+        includes.push_back(include.get_string());
+    }
+    return includes;
+}
+
+void LEAdvertisement1::LocalName(const std::string& name) {
     _properties["LocalName"] = SimpleDBus::Holder::create_string(name);
 }
 
-void LEAdvertisement1::SetAppearance(uint16_t appearance) {
+std::string LEAdvertisement1::LocalName() {
+    return _properties["LocalName"].get_string();
+}
+
+void LEAdvertisement1::Appearance(uint16_t appearance) {
     _properties["Appearance"] = SimpleDBus::Holder::create_uint16(appearance);
 }
 
-void LEAdvertisement1::SetDuration(uint16_t duration) {
+uint16_t LEAdvertisement1::Appearance() {
+    return _properties["Appearance"].get_uint16();
+}
+
+void LEAdvertisement1::Duration(uint16_t duration) {
     _properties["Duration"] = SimpleDBus::Holder::create_uint16(duration);
 }
 
-void LEAdvertisement1::SetTimeout(uint16_t timeout) {
+uint16_t LEAdvertisement1::Duration() {
+    return _properties["Duration"].get_uint16();
+}
+
+void LEAdvertisement1::Timeout(uint16_t timeout) {
     _properties["Timeout"] = SimpleDBus::Holder::create_uint16(timeout);
 }
 
-void LEAdvertisement1::SetSecondaryChannel(const std::string& channel) {
+uint16_t LEAdvertisement1::Timeout() {
+    return _properties["Timeout"].get_uint16();
+}
+
+void LEAdvertisement1::SecondaryChannel(const std::string& channel) {
     _properties["SecondaryChannel"] = SimpleDBus::Holder::create_string(channel);
 }
 
-void LEAdvertisement1::SetMinInterval(uint32_t interval) {
+std::string LEAdvertisement1::SecondaryChannel() {
+    return _properties["SecondaryChannel"].get_string();
+}
+
+void LEAdvertisement1::MinInterval(uint32_t interval) {
     _properties["MinInterval"] = SimpleDBus::Holder::create_uint32(interval);
 }
 
-void LEAdvertisement1::SetMaxInterval(uint32_t interval) {
+uint32_t LEAdvertisement1::MinInterval() {
+    return _properties["MinInterval"].get_uint32();
+}
+
+void LEAdvertisement1::MaxInterval(uint32_t interval) {
     _properties["MaxInterval"] = SimpleDBus::Holder::create_uint32(interval);
 }
 
-void LEAdvertisement1::SetTxPower(int16_t power) {
+uint32_t LEAdvertisement1::MaxInterval() {
+    return _properties["MaxInterval"].get_uint32();
+}
+
+void LEAdvertisement1::TxPower(int16_t power) {
     _properties["TxPower"] = SimpleDBus::Holder::create_int16(power);
+}
+
+int16_t LEAdvertisement1::TxPower() {
+    return _properties["TxPower"].get_int16();
+}
+
+void LEAdvertisement1::IncludeTxPower(bool include) {
+    _properties["IncludeTxPower"] = SimpleDBus::Holder::create_boolean(include);
+}
+
+bool LEAdvertisement1::IncludeTxPower() {
+    return _properties["IncludeTxPower"].get_boolean();
+}
+
+void LEAdvertisement1::Release() {
+    // This method is called when the advertisement is unregistered
+    // Implement any cleanup logic here
 }
